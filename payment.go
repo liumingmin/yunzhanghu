4d9770e0fd741dcb40ce2a65599ea6b6d295f0fd
liumingmin <443925902@qq.com>
2021-10-20 11:23:11 +0800

    feat: 补全下单逻辑

--- payment.go
+++ payment.go
@@ -204,3 +204,169 @@ func (y *Yunzhanghu) NotifyCallbackHandler(ctx *gin.Context) {
 
 	ctx.String(http.StatusOK, RESP_STR_SUCCESS)
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const (
+	queryAccountsURI = "/api/payment/v1/query-accounts"
+)
+
+type (
+	reqQueryAccounts struct {
+		DealerID string `json:"dealer_id"` // 商户ID
+	}
+	AccountBalanceData struct {
+		BrokerID         string  `json:"broker_id"`                 // 综合服务主体 ID
+		AcctBalance      float64 `json:"acct_balance,string"`       // 余额账户余额
+		AlipayBalance    float64 `json:"alipay_balance,string"`     // 支付宝余额
+		BankCardBalance  float64 `json:"bank_card_balance,string"`  // 银行卡余额
+		WxpayBalance     float64 `json:"wxpay_balance,string"`      // 微信余额
+		IsAlipay         bool    `json:"is_alipay"`                 // 是否开通支付宝通道
+		IsBankCard       bool    `json:"is_bank_card"`              // 是否开通银行卡通道
+		IsWxpay          bool    `json:"is_wxpay"`                  // 是否开通微信通道
+		RebateFeeBalance float64 `json:"rebate_fee_balance,string"` // 服务费返点余额
+		TotalBalance     float64 `json:"total_balance,string"`      // 总余额
+	}
+	retQueryAccounts struct {
+		CommonResponse
+		Data struct {
+			DealerInfos []*AccountBalanceData `json:"dealer_infos"`
+		} `json:"data"`
+	}
+)
+
+func (y *Yunzhanghu) QueryAccounts(ctx context.Context) ([]*AccountBalanceData, error) {
+	var (
+		input = &reqQueryAccounts{
+			DealerID: y.Dealer,
+		}
+		output  = new(retQueryAccounts)
+		apiName = "查询商户余额"
+	)
+	responseBytes, err := y.getJson(ctx, queryAccountsURI, apiName, input)
+	if err != nil {
+		return nil, err
+	}
+	if err = y.decodeWithError(responseBytes, output, apiName); err != nil {
+		return nil, err
+	}
+	return output.Data.DealerInfos, nil
+}
+
+const (
+	receiptFileURI = "/api/payment/v1/receipt/file"
+)
+
+type (
+	reqReceiptFile struct {
+		OrderID string `json:"order_id"`
+		Ref     string `json:"ref"`
+	}
+	ReceiptFileData struct {
+		ExpireTime string `json:"expire_time"` // 精确到秒 2020-09-05 18:36:37
+		FileName   string `json:"file_name"`   // 电子回单名称 w_140_receipt_20180726.png
+		URL        string `json:"url"`         // 电子回单的下载链接 https://xxxx
+	}
+	retReceiptFile struct {
+		CommonResponse
+		Data ReceiptFileData `json:"data"`
+	}
+)
+
+func (y *Yunzhanghu) ReceiptFile(ctx context.Context, orderId, ref string) (*ReceiptFileData, error) {
+	var (
+		input = &reqReceiptFile{
+			OrderID: orderId,
+			Ref:     ref,
+		}
+		output  = new(retReceiptFile)
+		apiName = "查询电子回单"
+	)
+	responseBytes, err := y.getJson(ctx, receiptFileURI, apiName, input)
+	if err != nil {
+		return nil, err
+	}
+	if err = y.decodeWithError(responseBytes, output, apiName); err != nil {
+		return nil, err
+	}
+	return &output.Data, nil
+}
+
+const (
+	cancelOrderURI = "/api/payment/v1/order/fail"
+)
+
+type (
+	reqCancelOrder struct {
+		Channel string `json:"channel"`  // 银行卡
+		OrderID string `json:"order_id"` // 202009010016562012987
+		Ref     string `json:"ref"`      // 176826728298982
+	}
+	CancelOrderData struct {
+		Ok bool `json:"ok,string"` // true
+	}
+	retCancelOrder struct {
+		CommonResponse
+		Data CancelOrderData `json:"data"`
+	}
+)
+
+func (y *Yunzhanghu) CancelOrder(ctx context.Context, orderId, ref, channel string) (*CancelOrderData, error) {
+	var (
+		input = &reqCancelOrder{
+			Channel: channel,
+			OrderID: orderId,
+			Ref:     ref,
+		}
+		output  = new(retCancelOrder)
+		apiName = "取消待打款订单"
+	)
+	responseBytes, err := y.postForm(ctx, cancelOrderURI, apiName, input)
+	if err != nil {
+		return nil, err
+	}
+	if err = y.decodeWithError(responseBytes, output, apiName); err != nil {
+		return nil, err
+	}
+	return &output.Data, nil
+}
+
+const (
+	queryVaAccountURI = "/api/payment/v1/va-account"
+)
+
+type (
+	reqQueryVaAccount struct {
+		BrokerID string `json:"broker_id"` // yiyun73
+		DealerID string `json:"dealer_id"` // 12345678
+	}
+	VaAccountData struct {
+		AcctName       string `json:"acct_name"`        // 云账户
+		AcctNo         string `json:"acct_no"`          // 2772510300399876543210
+		BankName       string `json:"bank_name"`        // 中国银行
+		DealerAcctName string `json:"dealer_acct_name"` // 企业测试名称
+	}
+	retQueryVaAccount struct {
+		CommonResponse
+		Data VaAccountData `json:"data"`
+	}
+)
+
+func (y *Yunzhanghu) QueryVaAccount(ctx context.Context) (*VaAccountData, error) {
+	var (
+		input = &reqQueryVaAccount{
+			BrokerID: y.Broker,
+			DealerID: y.Dealer,
+		}
+		output  = new(retQueryVaAccount)
+		apiName = "查询商户VA账户信息"
+	)
+	responseBytes, err := y.getJson(ctx, queryVaAccountURI, apiName, input)
+	if err != nil {
+		return nil, err
+	}
+	if err = y.decodeWithError(responseBytes, output, apiName); err != nil {
+		return nil, err
+	}
+	return &output.Data, nil
+}
